b585db05344740b46378c15cecd6ce34efa69cef
choilo11121999 <choilo11121999@gmail.com>
2021-05-11 14:37:56 +0700

    UI movie-detail

--- src/components/user/MovieDetail.js
+++ src/components/user/MovieDetail.js
@@ -26,7 +26,45 @@ const MoviePlaying = () => {
         </ul>
       </div>
       <div className="main">
-        main
+        <div className="main-header">
+          <h2>Nội dung phim</h2>
+        </div> 
+        <div className="main-body">
+          <div className="main-body-img">
+            {/* <img src={} /> */}
+            <p>IMG</p>
+          </div>
+          <div className="main-body-description">
+            <h3>Ten phim</h3>
+            <p><strong>Đạo diễn: </strong>Chọi</p>
+            <p><strong>Diễn viên: </strong>Chọi</p>
+            <p><strong>Thể loại: </strong>Chọi</p>
+            <p><strong>Khởi chiếu: </strong>Chọi</p>
+            <p><strong>Thời lượng: </strong>Chọi</p>
+            <p><strong>Ngôn ngữ: </strong>Chọi</p>
+            <p><strong>Rated: </strong></p>
+            <div className="main-body-btn">
+              <button className="btn btn-primary btn-sm">Like</button>
+              <button className="btn btn-danger">Mua vé</button>
+            </div>
+          </div>
+          <div className="detail-trailer">
+            <div className="form-check form-check-inline">
+              <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" />
+              <label className="form-check-label" for="inlineRadio1">Chi tiết</label>
+            </div>
+            <div className="form-check form-check-inline">
+              <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2" />
+              <label className="form-check-label" for="inlineRadio2">Trailer</label>
+            </div>
+            <div className="detail">
+              <p>Mo ta chi tiet phim</p>
+            </div>
+            <div className="trailer">
+              <p>Video</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
