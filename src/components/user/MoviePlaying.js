adb623c2361891036ad8f618337a15ecc7b5a85d
PKLogger <senseicuong@gmail.com>
2021-06-01 16:44:55 +0700

    Fix MoviePlaying Page

--- src/components/user/MoviePlaying.js
+++ src/components/user/MoviePlaying.js
@@ -45,30 +45,30 @@ const MoviePlaying = () => {
                   <a href="#">Bo gia</a>
                 </h2>
                 <div className="movie-info">
-                  <span className="movie-info-bold">Thể loại :</span>
+                  <span className="movie-info-bold">Thể loại: </span>
                   <span className="movie-info-normal">
                     Hành động, phiêu lưu
                   </span>
                 </div>
                 <div className="movie-info">
-                  <span className="movie-info-bold">Thời lượng :</span>
+                  <span className="movie-info-bold">Thời lượng: </span>
                   <span className="movie-info-normal">110 phút</span>
                 </div>
                 <div className="movie-info">
-                  <span className="movie-info-bold">Khởi chiếu :</span>
+                  <span className="movie-info-bold">Khởi chiếu: </span>
                   <span className="movie-info-normal">20-04-2021</span>
                 </div>
               </div>
               <ul className="add-to-links d-flex px-3">
                   <li>
-                      <button type="button" className="button btn-like">Like <span>1k</span></button>
+                    <button type="button" className="button btn-like">Like <span>1k</span></button>
                   </li>
                   <li>
-                      <button type="button" title="Mua vé" className="button btn-booking">
-                          <span>
-                              <span>Mua vé</span>
-                          </span>
-                      </button>
+                    <button type="button" title="Mua vé" className="button btn-booking">
+                      <span>
+                        <span>Mua vé</span>
+                      </span>
+                    </button>
                   </li>
               </ul>
             </li>
