50986bb575706378b6e8934afbc9fdef9958fc60
PKLogger <senseicuong@gmail.com>
2021-06-09 16:12:41 +0700

    Fix Chảng Info Page

--- src/components/auth/ChangeInfo.js
+++ src/components/auth/ChangeInfo.js
@@ -25,41 +25,51 @@ function ChangeInfo (props) {
         <div className="change-info-body my-3">
           <form onSubmit={handleSubmit}>
             <div className="form-group my-4">
-              <input
-                type="text" name="name" id="name"
-                placeholder="Nhập tên"
-                className="form-control" required
-                onChange={(e) => setName(e.target.value)}
-              />
+              <label>Tên:
+                <input
+                  type="text" name="name" id="name"
+                  placeholder="Nhập tên"
+                  className="form-control" required
+                  onChange={(e) => setName(e.target.value)}
+                />
+              </label>
             </div>
             <div className="form-group my-4">
-              <input type="email" name="email" id="email"
-                placeholder="Nhập email"
-                className="form-control" required
-                onChange={(e) => setEmail(e.target.value)}
-              />
+              <label>Email:
+                <input type="email" name="email" id="email"
+                  placeholder="Nhập email"
+                  className="form-control" required
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </label>
             </div>
             <div className="form-group my-4">
-              <input type="text" name="phone" id="phone"
-                placeholder="Nhập số điện thoại"
-                className="form-control" required
-                onChange={(e) => setPhone(e.target.value)} 
-              />
+              <label>Số điện thoại:
+                <input type="text" name="phone" id="phone"
+                  placeholder="Nhập số điện thoại"
+                  className="form-control" required
+                  onChange={(e) => setPhone(e.target.value)} 
+                />
+              </label>
             </div>
             <div className="form-group my-4">
-              <input type="text" name="address" id="address"
-                placeholder="Nhập địa chỉ"
-                className="form-control" required
-                onChange={(e) => setAddress(e.target.value)}
-              />
+              <label>Địa chỉ:
+                <input type="text" name="address" id="address"
+                  placeholder="Nhập địa chỉ"
+                  className="form-control" required
+                  onChange={(e) => setAddress(e.target.value)}
+                />
+              </label>
             </div>
             <div className="form-group my-4">
-              <input
-                type="text" name="city"id="city"
-                placeholder="Nhập thành phố"
-                className="form-control" required
-                onChange={(e) => setCity(e.target.value)}
-              />
+              <label>Thành phố:
+                <input
+                  type="text" name="city"id="city"
+                  placeholder="Nhập thành phố"
+                  className="form-control" required
+                  onChange={(e) => setCity(e.target.value)}
+                />
+              </label>
             </div>
             <div className="form-group my-4">
               <button type="submit" className="btn btn-danger btn-block btn-lg">
